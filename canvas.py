68a13d010a3993c3c3cbe7ba14a8851ae86464e0
sammyh27 <sammyh27@gmail.com>
2017-06-15 15:52:21 +0700

    canvas

--- canvas.py
+++ canvas.py
@@ -13,24 +13,23 @@ _CANVAS_STATE = [
 # ===========================================================================================================================
 
 class canvasssing_canvas(osv.Model):
-	_inherit = 'mail.thread'
 	_name = 'canvassing.canvas'
 	_description = 'Canvas'
 	
 # COLUMNS -------------------------------------------------------------------------------------------------------------------
 	
 	_columns = {
+		'name': fields.char('Name', readonly=True),
+		'state': fields.selection(_CANVAS_STATE, 'State', readonly=True),
 		'date_created': fields.datetime('Date Created'),
 		'date_depart': fields.datetime('Date Depart'),
 		'date_delivered': fields.datetime('Date Delivered'),
-		'name': fields.char('Name', readonly=True),
-		'driver1': fields.many2one('hr.employee', 'Driver 1'),
-		'driver2': fields.many2one('hr.employee', 'Driver 2'),
-		'state': fields.selection(_CANVAS_STATE, 'State', readonly=True),
-		'trip_expense': fields.one2many('canvassing.canvas.expense', 'canvas_id', 'Trip Expense'),
-		'fleet_vehicle': fields.many2one('fleet.vehicle', 'Vehicle'),
-		'canvas_stock_line': fields.one2many('canvassing.canvas.stock.line', 'canvas_id', 'Stock Line'),
-		'canvas_invoice_line': fields.one2many('canvassing.canvas.invoice.line', 'canvas_id', 'Invoice Line'),
+		'driver1_id': fields.many2one('hr.employee', 'Driver 1'),
+		'driver2_id': fields.many2one('hr.employee', 'Driver 2'),
+		'fleet_vehicle_id': fields.many2one('fleet.vehicle', 'Vehicle'),
+		'trip_expense_ids': fields.one2many('canvassing.canvas.expense', 'canvas_id', 'Trip Expense'),
+		'stock_line_ids': fields.one2many('canvassing.canvas.stock.line', 'canvas_id', 'Stock Line'),
+		'invoice_line_ids': fields.one2many('canvassing.canvas.invoice.line', 'canvas_id', 'Invoice Line'),
 	}
 	
 # DEFAULTS ------------------------------------------------------------------------------------------------------------------
@@ -40,13 +39,40 @@ class canvasssing_canvas(osv.Model):
 		'state': 'draft',
 	}
 
-# OVERRIDES -----------------------------------------------------------------------------------------------------------------
-
+# ACTIONS ------------------------------------------------------------------------------------------------------------------
+	
+	def action_set_on_the_way(self, cr, uid, ids, context={}):
+		for id in ids:
+			self.write(cr, uid, [id], {
+				'state': 'on_the_way',
+				'date_depart': datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+			}, context=context)
+	
+	def action_set_finish(self, cr, uid, ids, context={}):
+		for canvas_data in self.browse(cr, uid, ids):
+		# Cek minimal harus ada satu line yg is_executed nya true. Dan kl is_executed nya false, dia harus ada notes nya.
+			valid = False
+			for stock_line in canvas_data.stock_line_ids:
+				if stock_line.is_executed:
+					valid = True
+				else:
+					if stock_line.notes == False or stock_line.notes == "":
+						raise osv.except_osv(_('Stock Line Error'),_('Please fill the notes why it is not executed.'))
+			for invoice_line in canvas_data.invoice_line_ids:
+				if invoice_line.is_executed:
+					valid = True
+				else:
+					if invoice_line.notes == False or invoice_line.notes == "":
+						raise osv.except_osv(_('Invoice Line Error'),_('Please fill the notes why it is not executed.'))
+			if valid:
+				self.write(cr, uid, [canvas_data.id], {
+					'state': 'finished',
+					'date_delivered': datetime.today().strftime('%Y-%m-%d %H:%M:%S'),
+				}, context=context)
 
 # ===========================================================================================================================
 
 class canvasssing_canvas_stock_line(osv.Model):
-	_inherit = 'mail.thread'
 	_name = 'canvassing.canvas.stock.line'
 	_description = 'Canvas Stock Line'
 	
@@ -70,7 +96,6 @@ class canvasssing_canvas_stock_line(osv.Model):
 # ===========================================================================================================================
 
 class canvasssing_canvas_invoice_line(osv.Model):
-	_inherit = 'mail.thread'
 	_name = 'canvassing.canvas.invoice.line'
 	_description = 'Canvas Invoice Line'
 	
@@ -88,7 +113,6 @@ class canvasssing_canvas_invoice_line(osv.Model):
 # ===========================================================================================================================
 
 class canvasssing_canvas_expense(osv.Model):
-	_inherit = 'mail.thread'
 	_name = 'canvassing.canvas.expense'
 	_description = 'Canvas Expense'
 	
